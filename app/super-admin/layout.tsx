b744e488baaf2bf18484ef635b0ea84b696215f4
Shashwat <shashwat@e4a.in>
2026-02-11 21:55:13 +0530

    fix: add missing redirect import in super-admin layout

--- app/super-admin/layout.tsx
+++ app/super-admin/layout.tsx
@@ -1,5 +1,6 @@
 import { createClient } from '@/lib/supabase-server'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default async function SuperAdminLayout({
     children,
